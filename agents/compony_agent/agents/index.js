4fe5382380624a1dbb0584e79b1cc4a8399aa763
Mohammad Ibrahim <cshore.2750mi@gmail.com>
2026-02-01 09:46:35 +0530

    re-arrange backend and agents

--- agents/compony_agent/agents/index.js
+++
@@ -1,3 +0,0 @@
-export { understandingAgent } from "./understandingAgent.js";
-export { evaluationAgent } from "./evaluationAgent.js";
-export { decisionAgent } from "./decisionAgent.js";
--- agents/compony_agent/index.js
+++
@@ -1,5 +0,0 @@
-import { runBidOrchestrator } from "./orchestrator/bidOrchestrator.js";
-
-export async function evaluateBids(bids) {
-  return await runBidOrchestrator(bids);
-}
